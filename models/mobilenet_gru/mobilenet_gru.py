2cf2c3771ed5c997ada0440d4746392e896f54a3
THEX\sx564 <sushi5431@gmail.com>
2018-08-12 02:43:42 -0400

    add mobile net gru

---
+++ models/mobilenet_gru/mobilenet_gru.py
@@ -0,0 +1,156 @@
+"""
+A model that replace the top of DenseNet169 with GRU units
+"""
+import os
+import sys
+
+CURRENT_PATH = os.path.abspath(__file__)
+CURRENT_PATH = os.path.dirname(CURRENT_PATH)
+ROOT_PATH = os.path.dirname(CURRENT_PATH)
+ROOT_PATH = os.path.dirname(ROOT_PATH)
+sys.path.append(ROOT_PATH)
+
+import math
+
+import keras
+from keras.applications import imagenet_utils
+import numpy as np
+
+import mura_model
+
+
+class MobileNetGRU(mura_model.MuraModel):
+    """
+    A 16 layers VGGNet Model object that designed to work with MURA dataset.
+    """
+
+    def __init__(self, resize=False, weights=None, grayscale=False, lock_bottom=False, **kwargs):
+        super(MobileNetGRU, self).__init__(CURRENT_PATH, resize=resize, grayscale=grayscale)
+        self.img_size_vgg = 224
+        self.img_size = self.img_size_vgg if resize else self.img_size_origin
+        self.model = self.build_model(weights, lock_bottom)
+
+    def build_model(self, weights, lock_bottom):
+        """
+        Laying out the VGGNet model.
+        :param weights: pretrained weight to import.
+            Notes: if weight == "imagenet", will set image size to 224 and color to 3
+        :param lock_bottom: lock bottom layers weights.
+        :return: keras model
+        """
+        print("****** Building Model")
+
+        inputs = keras.layers.Input(
+            (11, self.img_size, self.img_size, self.color_channel)
+        )
+
+        custom_weights = weights and weights != "imagenet"
+        preload_weights = weights
+        if custom_weights:
+            preload_weights = None
+
+        preload_model = keras.applications.MobileNet(
+            input_shape=(self.img_size, self.img_size, self.color_channel),
+            weights=preload_weights,
+            include_top=False,
+            pooling="avg"
+        )
+
+        if lock_bottom:
+            for layer in preload_model.layers:
+                layer.trainable = False
+
+        output = keras.layers.TimeDistributed(preload_model)(inputs)
+
+        output = keras.layers.CuDNNGRU(12)(output)
+
+        output = keras.layers.Dense(
+            1,
+            activation="sigmoid",
+            name="predictions"
+        )(output)
+
+        model = keras.models.Model(
+            inputs=inputs,
+            outputs=[output],
+            name="DenseNet169GRU"
+        )
+
+        if custom_weights:
+            model.load_weights(weights, by_name=True)
+
+        return model
+
+    def load_and_process_image(self, path, imggen=None):
+        """
+        Load and preprocess a single image
+        Args:
+            path: path to image file.
+            imggen: Image Generator for performing image perturbation
+
+        Returns:
+            image in ndarray
+        """
+        img = super().load_and_process_image(path, imggen)
+        imagenet_utils.preprocess_input(img)
+        return img
+
+    def input_generator(self, df, batch_size, imggen=None):
+        """
+        Generator that yields a batch of images with their labels
+        Args:
+            df: Dataframe that contains all the images need to be loaded
+            batch_size: Maximum number of images in each batch
+            imggen: ImageDataGenerator used to apply perturbation. If None,
+                then no perturbation is applied.
+
+        Yields: List of training images and labels in a batch
+
+        """
+        df = df.groupby("study").agg({
+            "path": lambda x: x.tolist(),
+            "label": np.prod}
+        )
+        while True:
+            # shuffle the training set
+            df = df.sample(frac=1).reset_index(drop=True)
+            for g, batch in df.groupby(np.arange(len(df)) // batch_size):
+                # reset the index so that the index is based on position inside batch
+                batch.reset_index(inplace=True)
+                # seq_len = batch["path"].str.len().max()
+                seq_len = 11
+                effective_batch_size = min(batch_size, batch.shape[0])
+                imgs = np.zeros((
+                    effective_batch_size,
+                    seq_len,
+                    self.img_size,
+                    self.img_size,
+                    self.color_channel
+                ))
+                labels = []
+                for index, row in batch.iterrows():
+                    labels.append(row["label"])
+                    paths = row["path"]
+                    for i in range(len(paths)):
+                        img = self.load_and_process_image(paths[i], imggen)
+                        imgs[index, i, :, :, :] = img.reshape(1, 1, *img.shape)
+                yield np.asarray(imgs), np.asarray(labels)
+
+    def calc_steps(self, df, batch_size):
+        """
+        Calculate number of steps needed iterate over the dataframe
+        via generator in each epoch given the batch size.
+        Args:
+            df: dataframe to iterate
+            batch_size: batch size
+
+        Returns: number of steps
+
+        """
+        return math.ceil(len(df["study"].unique()) / batch_size)
+
+
+if __name__ == "__main__":
+    MobileNetGRU.TRAIN_PARSER.add_argument("--lock_bottom", action="store_true",
+                                           help="lock bottom layers weights.")
+    MobileNetGRU.train_from_cli()
